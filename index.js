449b3477a2f96b2910be00078fb963a0752bd31d
Vimal Chaudhary <86522515+Vimal1232@users.noreply.github.com>
2023-09-11 15:17:23 +0530

    Login

---
+++ index.js
@@ -0,0 +1,37 @@
+function vali() {
+  nam = /^[A-Za-z]{5,9}$/;
+  email = /^[A-Za-z_.-0-9]+@[a-zA-Z]+.[a-zA-Z]{2,3}$/;
+  mob = /^[0-9]{10}$/;
+
+  let inputElement = document.getElementById("user");
+  let inputElement2 = document.getElementById("email");
+  let inputElement3 = document.getElementById("phone");
+
+  if (document.form.first.value.search(nam) == -1) {
+    alert("I Need a Name To work With");
+    inputElement.addEventListener("focus", function () {
+      inputElement.style.border = "1px solid red";
+    });
+    inputElement.addEventListener("blur", function () {
+      inputElement.style.borderColor = "";
+    });
+    document.form.first.focus(nam);
+    return false;
+  } else if (document.form.second.value.search(email) == -1) {
+    alert("I need A Email, Darling");
+    inputElement2.addEventListener("focus", function () {
+      inputElement2.style.border = "1px solid red";
+    });
+    document.form.second.focus(email);
+    return false;
+  } else if (document.form.third.value.search(mob) == -1) {
+    alert("I Need A Mobile number , Boi ");
+    inputElement3.addEventListener("focus", function () {
+      inputElement3.style.border = "1px solid red";
+    });
+    document.form.third.focus(mob);
+    return false;
+  } else {
+    return true;
+  }
+}
